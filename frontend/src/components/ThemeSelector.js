7feee4116ed10ac411503c46d87c288ce4d27b16
Marawan <marawanabdelr@gmail.com>
2024-02-12 21:52:53 +0400

    Quick styles fixes

--- frontend/src/components/ThemeSelector.js
+++ frontend/src/components/ThemeSelector.js
@@ -14,8 +14,8 @@ const ThemeSelector = () => {
   return (
     <div className="theme-selector">
       <select className='select' id="theme-dropdown" onChange={handleChange}>
-        <option value="mapbox://styles/marawan1805/clh7miglu00vh01pg1vr50erd">Streets</option>
         <option value="mapbox://styles/marawan1805/clh7obmkt00ph01qt3fz40g7g">Light</option>
+        <option value="mapbox://styles/marawan1805/clh7miglu00vh01pg1vr50erd">Streets</option>
         <option value="mapbox://styles/marawan1805/clh7ogv2200un01pgepew5i0c">Dark</option>
       </select>
     </div>
